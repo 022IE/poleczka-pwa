032f75188eae8b6958496b09f82ab42447ecdbfe
022IE <90221328+022IE@users.noreply.github.com>
2026-09-16 10:32:57 +0200

    style: włącz korekty layoutu strony głównej

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App'
 import BuildStatus from './components/BuildStatus'
 import './styles.css'
 import './overrides.css'
+import './home-tuning.css'
 import './build-status.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
