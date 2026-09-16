08ee646df365ffe3f67c43bdcf374488714e4fc5
022IE <90221328+022IE@users.noreply.github.com>
2026-09-16 11:33:53 +0200

    fix: włącz grafikę referencyjną prawego panelu

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './overrides.css'
+import './quote-fix.css'
 import './build-status.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
