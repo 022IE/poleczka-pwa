4b91fc318e0fb1bca232a7e6b7d4b1d90f1a15be
022IE <90221328+022IE@users.noreply.github.com>
2026-09-16 18:04:35 +0200

    Podłącz style porównania okresów

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App'
 import './styles.css'
 import './overrides.css'
 import './quote-fix.css'
+import './comparison-controls.css'
 import './build-status.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
