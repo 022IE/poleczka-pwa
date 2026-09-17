7d14e1267e885a5ed626bb8bcc373ddd9182f0b7
022IE <90221328+022IE@users.noreply.github.com>
2026-09-17 15:19:50 +0200

    feat: load sales module styles

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './comparison-controls.css'
 import './vinted-dashboard.css'
 import './hourly-heatmap.css'
 import './build-status.css'
+import './sales.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
