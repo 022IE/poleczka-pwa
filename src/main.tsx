d593d39657932ece5c8a28a287e2b386cba3a61d
022IE <90221328+022IE@users.noreply.github.com>
2026-09-18 10:05:27 +0200

    style: enable global readability layer

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './vinted-dashboard.css'
 import './hourly-heatmap.css'
 import './build-status.css'
 import './sales.css'
+import './readability.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
